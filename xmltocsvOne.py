de1257027dcc3d4eb92b45f15f33821923e25e3f
Jonathan Valencia <77303685+Volkner90@users.noreply.github.com>
2023-12-01 12:05:57 -0600

    Add files via upload
    Script used to extract important data out of a XML file and save it to a CSV

---
+++ xmltocsvOne.py
@@ -0,0 +1,37 @@
+import xml.etree.ElementTree as ET
+import csv
+import os
+
+# Initialize an empty list to store the extracted data
+data = []
+
+# Iterate over all XML files in your folder
+xml_folder = r"C:\Users\jjcuevas\OneDrive - Intel Corporation\Documents\labelImg-master\datos"
+for xml_file in os.listdir(xml_folder):
+    if xml_file.endswith(".xml"):
+        xml_path = os.path.join(xml_folder, xml_file)
+        
+        # Parse the XML file
+        tree = ET.parse(xml_path)
+        root = tree.getroot()
+
+        # Extract information from each "object" element
+        for obj in root.findall(".//object"):
+            name = obj.find("name").text
+            xmin = obj.find(".//xmin").text
+            ymin = obj.find(".//ymin").text
+            xmax = obj.find(".//xmax").text
+            ymax = obj.find(".//ymax").text
+
+            # Get the path from the "path" element
+            path = root.find(".//path").text
+
+            # Append the extracted data to the list
+            data.append([path, xmin, ymin, xmax, ymax, name])
+
+# Write the extracted data to a CSV file
+csv_file = "labels.csv"
+with open(csv_file, "w", newline="") as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["Path", "xmin", "ymin", "xmax", "ymax", "Name"])
+    csv_writer.writerows(data)
